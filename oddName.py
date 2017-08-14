3e9d68298b9751a8b6677a702c8eda81d2b441c1
nath978 <nathan.reavell@my.jcu.edu.au>
2017-08-14 11:03:40 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,20 @@
 # """Nathan Reavell"""
+
+def main():
+
+    Name = input("What is your name? ")
+
+    while not is_valid(Name):
+        print("Name is invalid")
+        Name = input(">>> ")
+
+    for char in range(1,len(Name),2):
+        print(Name[char])
+
+def is_valid(Name):
+    if Name == "":
+        return False
+
+    return True
+
+main()
